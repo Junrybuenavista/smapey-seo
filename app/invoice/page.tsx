2c70889b2c1c67752c5c0f12be64750f4aeb8d56
Junry Buenavista <buenavistajunry@gmail.com>
2026-04-21 03:34:43 +0800

    channging links8

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: "Simple Invoicing Software for Small Business | Smapey",
   description: "Create invoices, send them instantly, and get paid faster with Smapey. Simple invoicing software for freelancers and small businesses. Start free.",
     alternates: {
-    canonical: "https://smapey.com",
+    canonical: "https://smapey.com/invoice",
   },
 }
 
